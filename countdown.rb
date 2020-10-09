51745b5a8c481f2020387dfa99338e6eaff9b1ea
AbysmalPsycho <rischthefisch@gmail.com>
2020-10-08 18:50:14 -0700

    Boom easy

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(x)
+    x = 10
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    # binding.pry
+    end
+    "HAPPY NEW YEAR!"
+
 end
+
+def countdown_with_sleep(x)
+    x = 10
+    while x > 0
+        puts "#{x} SECOND(S)!"
+
+        sleep 1 
+        x -= 1
+    # binding.pry
+    end
+    "HAPPY NEW YEAR!"
+
+
+end 
